7a8ed9772f44aada78e8095fdd48fc3ce76cda22
IgorNovozhilov <maildrakon@gmail.com>
2020-09-10 01:07:56 +0300

    test1

---
+++ src/external.js
@@ -0,0 +1,2 @@
+export { oom } from '@notml/core'
+export { default as Dexie } from 'dexie'
---
+++ src/service-worker-register.js
@@ -0,0 +1,17 @@
+const { navigator } = window
+
+
+if ('serviceWorker' in navigator) {
+  (async () => {
+    const { serviceWorker } = navigator
+    const regSW = await serviceWorker.getRegistration()
+
+
+    if (regSW && regSW.active && !regSW.active.scriptURL.endsWith('{{ build_number }}')) {
+      await regSW.unregister('/')
+    }
+    await serviceWorker.register('/assets/offline.js?v={{ build_number }}', { scope: '/' })
+
+    console.log('serviceWorker succeeded: v={{ build_number }}')
+  })().catch(console.error)
+}
