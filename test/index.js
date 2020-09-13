6b069997f9437d37966fbb64fda5c3b1ea9daed7
IgorNovozhilov <maildrakon@gmail.com>
2020-09-14 00:01:00 +0300

    test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,15 @@
 import './emulateDOM.js'
 import { Test } from '@nodutilus/test'
 import { Tx64url } from './lib/x64url.js'
+import { Tdataurl } from './lib/dataurl.js'
 
 
 /** Общий тестовый класс */
 class TestQ3S extends Test {
 
   static x64url = Tx64url
+
+  static dataurl = Tdataurl
 
 }
 
--- test/lib/x64url.js
+++ test/lib/x64url.js
@@ -1,12 +1,12 @@
 import { Test, assert } from '@nodutilus/test'
 import {
   intToX16Pos2, intToX16Pos3, intToX64, x64ToInt,
-  intToX64Pos2, encodeURLx64, decodeURLx64, testURLChars, decodeURISearch
+  intToX64Pos2, encodeURLx64, decodeURLx64
 } from '../../lib/x64url.js'
 
 
 /**
- * Проверка работы со сжатием URL и допустимыми символами URL
+ * Проверка работы со сжатием URL
  */
 export class Tx64url extends Test {
 
@@ -73,41 +73,6 @@ export class Tx64url extends Test {
     assert.deepEqual(decodeURLx64('____'), new Uint8Array([255, 255, 255]))
     assert.deepEqual(decodeURLx64('0gYg.ff'), new Uint8Array([1, 15, 16, 255]))
     assert.deepEqual(decodeURLx64('0gYg.1'), new Uint8Array([1, 15, 16, 1]))
-  }
-
-  /** Проверяет что строка содержит только разрешенные для URL символы */
-  ['testURLChars']() {
-    assert.equal(testURLChars('Привет'), false)
-    assert.equal(testURLChars('QabgJt61Qo.2'), true)
-    assert.equal(testURLChars('____'), true)
-    assert.equal(testURLChars('0gYg.ff'), true)
-    assert.equal(testURLChars('0gYg.1'), true)
-    assert.equal(testURLChars('q3s.github.io/test/api'), true)
-
-    const x64alphabet = '0123456789' +
-      'abcdefghijklmnopqrstuvwxyz' +
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-      '-_.'
-
-    assert.equal(testURLChars(x64alphabet), true)
-  }
-
-  /** Проверка преобразования параметров поиска в url */
-  ['decodeURISearch']() {
-    const { URL, URLSearchParams } = window
-    const url1 = new URL('http://q3s.github.io/')
-    const url2 = new URL('http://q3s.github.io/')
-
-    url1.hash = new URLSearchParams('a=q3s.github.io/api/')
-    url2.hash = new URLSearchParams('a=q3s.github.io/апи:?/-_.!~*\'()')
-
-    const url1Result = decodeURISearch(url1.href)
-    const url2Result = decodeURISearch(url2.href)
-    const eURI = encodeURI(decodeURIComponent(url2Result))
-
-    assert.equal(url1Result, 'http://q3s.github.io/#a=q3s.github.io/api/')
-    assert.equal(url2Result, 'http://q3s.github.io/#a=q3s.github.io/%D0%B0%D0%BF%D0%B8%3A%3F/-_.%21%7E*%27%28%29')
-    assert.equal(eURI, 'http://q3s.github.io/#a=q3s.github.io/%D0%B0%D0%BF%D0%B8:?/-_.!~*\'()')
   }
 
 }
