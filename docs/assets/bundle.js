0c04438751b288444a56b6e363e38329e0f9a1e7
IgorNovozhilov <maildrakon@gmail.com>
2020-09-17 23:41:59 +0300

    test

--- docs/assets/bundle.js
+++ docs/assets/bundle.js
@@ -7,11 +7,7 @@ const { navigator } = window;
 if ('serviceWorker' in navigator) {
   (async () => {
     const { serviceWorker } = navigator;
-    const regSW = await serviceWorker.getRegistration();
-    if (regSW && regSW.active && !regSW.active.scriptURL.endsWith('{{ build_number }}')) {
-      await regSW.unregister('/');
-    }
-    await serviceWorker.register('/offline.js?v={{ build_number }}', { scope: '/' });
+    await serviceWorker.register('/offline.js', { scope: '/' });
     console.log('serviceWorker succeeded: v={{ build_number }}');
   })().catch(console.error);
 }
--- docs/offline.js
+++ docs/offline.js
@@ -2,16 +2,17 @@
 ---
 {% capture build_number %}{{ site.github.build_revision }}{{ site.time | date: '%Y%m%d%H%M%S' }}{% endcapture %}
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open('{{ build_number }}').then((cache) => {
       return cache.addAll([
         './',
+        './offline.js',
+        './q3s.webmanifest?v={{ build_number }}',
         './assets/bundle.css?v={{ build_number }}',
         './assets/bundle.js?v={{ build_number }}',
         './assets/check-and-redirect.js?v={{ build_number }}',
         './assets/external.js?v={{ build_number }}',
-        './offline.js?v={{ build_number }}',
-        './q3s.webmanifest?v={{ build_number }}',
         './assets/png/favicon32.png?v={{ build_number }}',
         './assets/png/favicon64.png?v={{ build_number }}',
         './assets/png/favicon128.png?v={{ build_number }}',
@@ -23,7 +24,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
+      return Promise.all(keyList.map(key => {
         if (key !== '{{ build_number }}') {
           return caches.delete(key)
         }
@@ -32,18 +33,23 @@ self.addEventListener('activate', (event) => {
   );
 });
 self.addEventListener('fetch', (event) => {
+  if (new URL(event.request.url).pathname === '/') {
+    fetch(`./version.json?t=${new Date().toJSON()}`)
+      .then(data => data.json())
+      .then(({ buildNumber }) => {
+        if (buildNumber !== '{{ build_number }}') {
+          return self.registration.update()
+        }
+      }).catch(error => console.error(error, error.stack));
+  }
   event.respondWith(
-    (new URL(event.request.url).pathname) === '/'
-      ? fetch(event.request).catch(() => {
-        return caches.match(event.request)
-      })
-      : caches.match(event.request).then((response) => {
-        return response || fetch(event.request).then((response) => {
-          return caches.open('{{ build_number }}').then((cache) => {
-            cache.put(event.request, response.clone());
-            return response
-          })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((response) => {
+        return caches.open('{{ build_number }}').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response
         })
       })
+    })
   );
 });
