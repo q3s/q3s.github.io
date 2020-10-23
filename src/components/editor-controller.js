c175d4bb7f216c4264b534d4b9ea821587d4e971
IgorNovozhilov <maildrakon@gmail.com>
2020-10-23 13:48:36 +0300

    +

--- src/components/editor-controller.js
+++ src/components/editor-controller.js
@@ -4,6 +4,26 @@ oom.define(class Q3SEditorController extends HTMLElement {
 
   static tagName = 'q3s-editor-controller'
 
-  template = oom()
+  template = oom.div({ class: 'mdc-select mdc-select--filled' }, oom
+    .div({ class: 'mdc-select__anchor' }, oom
+      .span({ class: 'mdc-select__ripple' })
+      .span({ class: 'mdc-select__selected-text' })
+      .span({ class: 'mdc-select__dropdown-icon' }, oom
+        .svg({ class: 'mdc-select__dropdown-icon-graphic', viewBox: '7 10 10 5' }, oom
+          .polygon({
+            'class': 'mdc-select__dropdown-icon-inactive',
+            'stroke': 'none',
+            'fill-rule': 'evenodd',
+            'points': '7 10 12 15 17 10'
+          })
+          .polygon({
+            'class': 'mdc-select__dropdown-icon-active',
+            'stroke': 'none',
+            'fill-rule': 'evenodd',
+            'points': '7 15 12 10 17 15'
+          })))
+      .span('Тип данных', { class: 'mdc-floating-label' })
+      .span({ class: 'mdc-line-ripple' }))
+    .div({ class: 'mdc-select__menu mdc-menu mdc-menu-surface mdc-menu-surface--fullwidth' }))
 
 })
