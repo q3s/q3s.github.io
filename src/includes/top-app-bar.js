d6f0c2577c5ade41eb1362e1a3817d973e7cd103
Igor Novozhilov <maildrakon@gmail.com>
2020-10-30 01:33:55 +0300

    swype (#6)

--- src/includes/top-app-bar.js
+++ src/includes/top-app-bar.js
@@ -1,6 +1,7 @@
 import { MDCTopAppBar } from '@material/top-app-bar'
 import { q3sDrawer } from './navigation-drawer.js'
 
+const { location, history } = window
 const q3sTopAppBar = MDCTopAppBar.attachTo(document.querySelector('.q3s-top-app-bar'))
 
 
@@ -9,4 +10,30 @@ q3sTopAppBar.listen('MDCTopAppBar:nav', () => {
   q3sDrawer.open = !q3sDrawer.open
 })
 
-window.addEventListener('hashchange', () => { q3sDrawer.open = false }, false)
+window.addEventListener('hashchange', () => {
+  q3sDrawer.open = false
+}, false)
+
+window.addEventListener('touch:swype:left_to_right', () => {
+  q3sDrawer.open = true
+}, false)
+
+window.addEventListener('touch:swype:right_to_left', e => {
+  if (q3sDrawer.open) {
+    q3sDrawer.open = false
+  } else {
+    if ((location.hash || '#') !== '#') {
+      if (history.length > 1 && location.hash === '#scanner') {
+        history.back()
+      } else {
+        window.location = '/#'
+      }
+    }
+  }
+}, false)
+
+window.addEventListener('touch:swype:bottom_to_up', () => {
+  if (location.hash !== '#scanner') {
+    window.location = '/#scanner'
+  }
+}, false)
