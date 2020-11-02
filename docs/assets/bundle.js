fdf105e3e2ccf4f2d9feaa1e467ba2d15e62c8f8
IgorNovozhilov <maildrakon@gmail.com>
2020-11-02 22:39:13 +0300

    swype - hypotenuseStep

--- docs/assets/bundle.js
+++ docs/assets/bundle.js
@@ -89,6 +89,7 @@ oom.define('q3s-navigation-router', class Q3SNavigationRouter extends HTMLElemen
 
 const { screen, document: document$1 } = window;
 const swypeStep = (screen.width > screen.height ? screen.height : screen.width) / 15;
+const hypotenuseStep = swypeStep / 3;
 let touchStartX = 0;
 let touchStartY = 0;
 let touchEnd = false;
@@ -98,15 +99,22 @@ document$1.body.addEventListener('touchstart', function touchstart(e) {
 }, false);
 document$1.body.addEventListener('touchmove', function touchmove(e) {
   if (!touchEnd) {
-    if (e.targetTouches[0].clientX - touchStartX > swypeStep) {
-      window.dispatchEvent(new Event('touch:swype:left_to_right'));
-      touchEnd = true;
-    } else if (touchStartX - e.targetTouches[0].clientX > swypeStep) {
-      window.dispatchEvent(new Event('touch:swype:right_to_left'));
-      touchEnd = true;
-    } else if (touchStartY - e.targetTouches[0].clientY > swypeStep) {
-      window.dispatchEvent(new Event('touch:swype:bottom_to_up'));
-      touchEnd = true;
+    const absX = Math.abs(touchStartX - e.targetTouches[0].clientX);
+    const absY = Math.abs(touchStartY - e.targetTouches[0].clientY);
+    if (absX > hypotenuseStep && absY > hypotenuseStep) {
+      touchStartX = e.targetTouches[0].clientX;
+      touchStartY = e.targetTouches[0].clientY;
+    } else {
+      if (e.targetTouches[0].clientX - touchStartX > swypeStep) {
+        window.dispatchEvent(new Event('touch:swype:left_to_right'));
+        touchEnd = true;
+      } else if (touchStartX - e.targetTouches[0].clientX > swypeStep) {
+        window.dispatchEvent(new Event('touch:swype:right_to_left'));
+        touchEnd = true;
+      } else if (touchStartY - e.targetTouches[0].clientY > swypeStep) {
+        window.dispatchEvent(new Event('touch:swype:bottom_to_up'));
+        touchEnd = true;
+      }
     }
   }
 }, false);
