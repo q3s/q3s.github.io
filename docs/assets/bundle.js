b4b7f79cc70137c8d3e7c549d033e5bc026bcc1d
IgorNovozhilov <maildrakon@gmail.com>
2020-11-08 15:20:22 +0300

    test

--- docs/assets/bundle.js
+++ docs/assets/bundle.js
@@ -187,16 +187,37 @@ const { navigator: navigator$1 } = window;
 oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
   _videoConstraints = { video: { facingMode: 'environment' } }
   _codeReader = new ZXing.BrowserMultiFormatReader()
+  _resizeTimeout = null
   template = () => oom
-    .div({ class: 'q3s-code-scanner__video-container' }, oom
-      .video({ class: 'q3s-code-scanner__video' }, elm => { this._videoElm = elm; }))
+    .div({ class: 'q3s-code-scanner__video-container' },
+      oom
+        .video({ class: 'q3s-code-scanner__video' },
+          elm => { this._videoElm = elm; }
+        ),
+      elm => { this._videoContainerElm = elm; }
+    )
+  constructor() {
+    super();
+    this._onResize = () => this.resizeEven();
+  }
   connectedCallback() {
     this.startVideo();
+    window.addEventListener('resize', this._onResize, false);
   }
   disconnectedCallback() {
     this.stopVideo();
+    window.removeEventListener('resize', this._onResize);
+    this._codeReader.reset();
     if (this._moreErrBtn) {
       delete this._moreErrBtn.onclick;
+    }
+  }
+  resizeEven() {
+    if (!this._resizeTimeout) {
+      this._resizeTimeout = setTimeout(() => {
+        this._resizeTimeout = null;
+        this.alignmentVideo();
+      }, 100);
     }
   }
   startVideo() {
@@ -205,16 +226,35 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
         .then(stream => {
           [this._videoTrack] = stream.getTracks();
           if (this.isConnected) {
-            debugger
             this._videoElm.srcObject = stream;
             this._videoElm.play();
+            this._videoElm.addEventListener('canplay', (self => function _handler(e) {
+              e.currentTarget.removeEventListener('canplay', _handler);
+              self.alignmentVideo();
+            })(this));
           } else {
             this.stopVideo();
           }
         })
-        .catch(error => this.decodeVideoError(error));
+        .catch(error => this.videoCameraError(error));
     } catch (error) {
-      this.decodeVideoError(error);
+      this.videoCameraError(error);
+    }
+  }
+  alignmentVideo() {
+    const { clientHeight: chvc, clientWidth: cwvc } = this._videoContainerElm;
+    const { clientHeight: chv, clientWidth: cwv } = this._videoElm;
+    const diffHeight = (chv - chvc) / 2 ^ 0;
+    const diffWidth = (cwv - cwvc) / 2 ^ 0;
+    if (diffHeight > 0) {
+      this._videoElm.style.marginTop = `-${diffHeight}px`;
+    } else {
+      this._videoElm.style.marginTop = '';
+    }
+    if (diffWidth > 0) {
+      this._videoElm.style.marginLeft = `-${diffWidth}px`;
+    } else {
+      this._videoElm.style.marginLeft = '';
     }
   }
   stopVideo() {
@@ -224,9 +264,9 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
       this._videoElm.srcObject = null;
     }
   }
-  decodeVideoError(error) {
+  videoCameraError(error) {
     const message = error + '\n' + (error.stack || '');
-    const content = oom()
+    const content = oom('div', { class: 'mdc-card' })
       .div({ class: 'q3s-code-scanner__card-content' }, oom
         .p('Не удалось получить доступ к камере.')
         .p('Вы можете загрузить изображение из галереи.')
@@ -237,9 +277,9 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
         .button({ class: 'mdc-button mdc-card__action mdc-card__action--button' }, oom
           .div({ class: 'mdc-button__ripple' })
           .span({ class: 'mdc-button__label' }, 'Подробнее...')), elm => { this._moreErrBtn = elm; });
-    this._card.innerHTML = '';
-    this._card.classList.remove('q3s-code-scanner__card--video');
-    this._card.append(content.dom);
+    this.stopVideo();
+    this.innerHTML = '';
+    this.append(content.dom);
     MDCRipple.attachTo(this._moreErrBtn);
     this._moreErrBtn.onclick = () => {
       this._errorElm.classList.toggle('q3s-code-scanner__hide');
--- src/components/code-scanner.js
+++ src/components/code-scanner.js
@@ -11,14 +11,25 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
 
   _codeReader = new ZXing.BrowserMultiFormatReader()
 
+  _resizeTimeout = null
+
   template = () => oom
-    .div({ class: 'q3s-code-scanner__video-container' }, oom
-      .video({ class: 'q3s-code-scanner__video' }, elm => { this._videoElm = elm }))
-  // .div({ class: 'mdc-card q3s-code-scanner__card' }, oom
-  //   .div(), elm => { this._card = elm })
+    .div({ class: 'q3s-code-scanner__video-container' },
+      oom
+        .video({ class: 'q3s-code-scanner__video' },
+          elm => { this._videoElm = elm }
+        ),
+      elm => { this._videoContainerElm = elm }
+    )
+
+  constructor() {
+    super()
+    this._onResize = () => this.resizeEven()
+  }
 
   connectedCallback() {
     this.startVideo()
+    window.addEventListener('resize', this._onResize, false)
     // try {
     //   this._codeReader.decodeFromConstraints(this._videoConstraints, this._videoElm,
     //     (result, error) => {
@@ -39,9 +50,19 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
 
   disconnectedCallback() {
     this.stopVideo()
-    // this._codeReader.reset()
+    window.removeEventListener('resize', this._onResize)
+    this._codeReader.reset()
     if (this._moreErrBtn) {
       delete this._moreErrBtn.onclick
+    }
+  }
+
+  resizeEven() {
+    if (!this._resizeTimeout) {
+      this._resizeTimeout = setTimeout(() => {
+        this._resizeTimeout = null
+        this.alignmentVideo()
+      }, 100)
     }
   }
 
@@ -51,16 +72,37 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
         .then(stream => {
           [this._videoTrack] = stream.getTracks()
           if (this.isConnected) {
-            debugger
             this._videoElm.srcObject = stream
             this._videoElm.play()
+            this._videoElm.addEventListener('canplay', (self => function _handler(e) {
+              e.currentTarget.removeEventListener('canplay', _handler)
+              self.alignmentVideo()
+            })(this))
           } else {
             this.stopVideo()
           }
         })
-        .catch(error => this.decodeVideoError(error))
+        .catch(error => this.videoCameraError(error))
     } catch (error) {
-      this.decodeVideoError(error)
+      this.videoCameraError(error)
+    }
+  }
+
+  alignmentVideo() {
+    const { clientHeight: chvc, clientWidth: cwvc } = this._videoContainerElm
+    const { clientHeight: chv, clientWidth: cwv } = this._videoElm
+    const diffHeight = (chv - chvc) / 2 ^ 0
+    const diffWidth = (cwv - cwvc) / 2 ^ 0
+
+    if (diffHeight > 0) {
+      this._videoElm.style.marginTop = `-${diffHeight}px`
+    } else {
+      this._videoElm.style.marginTop = ''
+    }
+    if (diffWidth > 0) {
+      this._videoElm.style.marginLeft = `-${diffWidth}px`
+    } else {
+      this._videoElm.style.marginLeft = ''
     }
   }
 
@@ -72,9 +114,9 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
     }
   }
 
-  decodeVideoError(error) {
+  videoCameraError(error) {
     const message = error + '\n' + (error.stack || '')
-    const content = oom()
+    const content = oom('div', { class: 'mdc-card' })
       .div({ class: 'q3s-code-scanner__card-content' }, oom
         .p('Не удалось получить доступ к камере.')
         .p('Вы можете загрузить изображение из галереи.')
@@ -86,9 +128,10 @@ oom.define('q3s-code-scanner', class Q3SCodeScanner extends HTMLElement {
           .div({ class: 'mdc-button__ripple' })
           .span({ class: 'mdc-button__label' }, 'Подробнее...')), elm => { this._moreErrBtn = elm })
 
-    this._card.innerHTML = ''
-    this._card.classList.remove('q3s-code-scanner__card--video')
-    this._card.append(content.dom)
+    this.stopVideo()
+
+    this.innerHTML = ''
+    this.append(content.dom)
 
     MDCRipple.attachTo(this._moreErrBtn)
     this._moreErrBtn.onclick = () => {
